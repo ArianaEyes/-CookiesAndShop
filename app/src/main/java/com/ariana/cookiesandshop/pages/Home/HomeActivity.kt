84c01d4355e76b6eaf07bf59e99be3e23e7356df
Ariana1825 <apreyes18@outlook.com>
2026-05-18 13:48:18 -0500

    actualizando otra importacion4

--- app/src/main/java/com/ariana/cookiesandshop/pages/Home/HomeActivity.kt
+++ app/src/main/java/com/ariana/cookiesandshop/pages/Home/HomeActivity.kt
@@ -26,8 +26,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import coil.compose.AsyncImage
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
