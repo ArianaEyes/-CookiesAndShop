95a512c61cb3bb3d606cbfc564514fdbc0c02df0
Ariana1825 <apreyes18@outlook.com>
2026-05-18 13:47:32 -0500

    actualizando importaciones

--- app/src/main/java/com/ariana/cookiesandshop/pages/Home/HomeActivity.kt
+++ app/src/main/java/com/ariana/cookiesandshop/pages/Home/HomeActivity.kt
@@ -55,7 +55,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.ariana.cookiesandshop.models.Postres
 import com.ariana.cookiesandshop.pages.MostrarActivity
